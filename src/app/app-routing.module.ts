37286f67e515deb2fddfa407543352e1cfc27386
HABIBA <habiba.hamad97@eng-st.cu.edu.eg>
2020-10-05 06:51:34 +0200

    form startup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', 
+      // loadChildren: './components/sign/sign.module#SignModule' 
+      loadChildren: () => import('./components/sign/sign.module').then(mod => mod.SignModule)
 
+    },
+
+   
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignComponent } from './components/sign/sign.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
